ece30cdf7b9e4f0156d96a409381301a7515ac0f
Munoz Flores <h616554@honeywell.com>
2025-08-07 09:31:53 -0600

    papu!! despierta papu!!

--- app/src/main/java/com/example/pantallas/MainActivity.kt
+++ app/src/main/java/com/example/pantallas/MainActivity.kt
@@ -1,31 +1,21 @@
 package com.example.pantallas
 
-import android.content.Context
 import android.content.Intent
-import android.content.pm.ActivityInfo
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.View
-import android.view.inputmethod.InputBinding
-import android.widget.Button
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.pantallas.databinding.ActivityMainBinding
-import com.google.zxing.integration.android.IntentIntegrator
-import com.google.zxing.integration.android.IntentResult
-import com.journeyapps.barcodescanner.ScanContract
-import com.journeyapps.barcodescanner.ScanIntentResult
-import com.journeyapps.barcodescanner.ScanOptions
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding.scannerbtn.setOnClickListener {
+            Intent(this,Activity2::class.java)
+        }
 
     }
 }
